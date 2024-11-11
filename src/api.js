dcc75a8735f373d02a67a72a75b493bceb7315e1
francisrjs <franrojas331@gmail.com>
2024-11-11 18:31:28 -0300

    add Button,search and API for temperature actual

---
+++ src/api.js
@@ -0,0 +1,126 @@
+import './style.css';
+import moment from 'moment';
+
+const api = "https://archive-api.open-meteo.com/v1/archive?";
+const apiLoc = "https://geocoding-api.open-meteo.com/v1/search?&count=1&language=en&format=json&name=";
+const apiCurrentBase = "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m&";
+
+function initApi(api, location, days) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const apiLocation = apiLoc + location;
+            const response = await fetch(apiLocation);
+  
+            if (!response.ok) {
+                throw new Error(`Error fetching location: ${response.status}`);
+            }
+  
+            const data = await response.json();
+  
+            if (data.results && data.results.length > 0) {
+                const firstResult = data.results[0];
+                const latitude = firstResult.latitude;
+                const longitude = firstResult.longitude;
+                
+               
+                const apiCurrent = `${apiCurrentBase}latitude=${latitude}&longitude=${longitude}`;
+                console.log("apiCurrent URL:", apiCurrent);
+                putLastTemperature(apiCurrent,location);
+                const apiPos = `${api}latitude=${latitude}&longitude=${longitude}&`;
+                const endDate = moment().format("YYYY-MM-DD");
+                const startDate = moment().subtract(days, 'days').format("YYYY-MM-DD");
+  
+                const resultApi = `${apiPos}start_date=${startDate}&end_date=${endDate}&daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean`;
+                resolve({ resultApi, apiCurrent }); // Devolvemos ambas URLs
+            } else {
+                reject(new Error("No results found in API response."));
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+async function getTime(location, days, element) {
+    try {
+        const { resultApi } = await initApi(api, location, days);
+        console.log("Historical API URL:", resultApi);
+        
+        const response = await fetch(resultApi);
+        
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+        const timeData = await response.json();
+        console.log("Weather Data:", timeData);
+        displayWheaterData(timeData.daily, element);
+    } catch (error) {
+        console.error("Error al obtener el tiempo:", error);
+        element.textContent = "Error al obtener los datos";
+    }
+}
+
+function displayWheaterData(hourlyData, element) {
+    element.textContent = "";  
+
+    hourlyData.time.forEach((time, index) => {
+        let card = document.createElement("div");
+        card.classList.add("card", "mb-3", "p-3");
+
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+
+        let titleIndex = document.createElement("h3");
+        let temperatureMaxP = document.createElement("p");
+        let temperatureMinP = document.createElement("p");
+        let temperatureMidP = document.createElement("p");
+
+        temperatureMidP.classList.add("card-text");
+        temperatureMinP.classList.add("card-text");
+        temperatureMaxP.classList.add("card-text");
+        titleIndex.classList.add("card-title");
+
+        titleIndex.innerHTML = `<i class="bi bi-calendar"></i> ${time}`;
+        temperatureMaxP.textContent = `Temperature Max: ${hourlyData.temperature_2m_max[index]} °C`;
+        temperatureMinP.textContent = `Temperature Min: ${hourlyData.temperature_2m_min[index]} °C`;
+        temperatureMidP.textContent = `Temperature Mid: ${hourlyData.temperature_2m_mean[index]} °C`;
+        titleIndex.setAttribute("id", "title-data");
+
+        cardBody.appendChild(titleIndex);
+        cardBody.appendChild(temperatureMaxP);
+        cardBody.appendChild(temperatureMinP);
+        cardBody.appendChild(temperatureMidP);
+        card.appendChild(cardBody);
+        element.appendChild(card);
+    });
+}
+
+async function putLastTemperature(api) {
+    let titleCity= document.querySelector("#city");
+    try {
+      const response = await fetch(api);
+  
+      if (!response.ok) {
+        if (response.status === 404) {
+          throw new Error(`No se encontró la ubicación`);
+        } else {
+          throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+      }
+  
+      const currentTemperature = await response.json();
+
+      const temperatures = currentTemperature.hourly.temperature_2m;
+
+      const latestTemperature = temperatures[temperatures.length - 1];
+
+
+
+      titleCity.innerHTML= `<i class="bi bi-building-fill"></i>${titleCity.textContent}, the last temperature:${latestTemperature}°C`;
+    } catch (error) {
+      console.error("Error al obtener el tiempo actual:", error);
+      titleCity.textContent = `Error: ${error.message}`;
+    }
+  }
+
+export { getTime };
--- src/index.js
+++ src/index.js
@@ -1,83 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
-let api="https://archive-api.open-meteo.com/v1/archive?"
-var moment= require("moment");
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { getTime } from './api';
 
+const button= document.querySelector("#btn-search");
 const jsonContainer=document.querySelector("#json-api-time");
-function getPos(location,latitude){
-    switch(location){
-        case "buenos-aires":
-           return  (latitude)? -34.6131 : -58.3772; //latitude / longitude
-        case "london":
-            return (latitude)? 51.50: -0.12;//latitude / longitude
+button.addEventListener("click",()=>{
+    jsonContainer.textContent=''; 
+    let search= document.querySelector("#input-search");
+    let searchText= search.value.toUpperCase();
+    let titleCity= document.querySelector("#city");
+    titleCity.innerHTML = `<i class="bi bi-building-fill"></i> ${searchText}`;
+    let searchDay= document.querySelector("#days-input");
+    let days=searchDay.value;
+    if (days>=61){
+      alert("Error: Days>60")
+    }else{
+      getTime(searchText,days,jsonContainer);
     }
     
-}
-function initApi(api,location,days){
-    let positionLatitude=getPos(location,1);
-    let positionLongitude=getPos(location,0);
-    let apiPos= api + "latitude=" + positionLatitude +"&"+"longitude="+positionLongitude+"&";
-    let endDate=moment().format("YYYY-MM-DD");
-    let startDate= moment().subtract(days,'days').format("YYYY-MM-DD");
-    console.log(startDate);
-    console.log(endDate);
-    let resultApi=apiPos + "start_date=" + startDate + "&" + "end_date=" + endDate + "&" + "daily=temperature_2m_max,temperature_2m_min,temperature_2m_mean";
-    console.log(resultApi);
-    return resultApi;
-}
-
-async function getTime(location,days,element){
-    try{
-        let apiLocation=initApi(api,location,days);
-        let response= await fetch(apiLocation);
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.status}`);
-        }
-        let timeData = await response.json();
-        displayWheaterData(timeData.daily,element);
-    }
-  catch(error){
-    console.error("Error al obtener el tiempo:", error);
-    element.textContent = "Error al obtener los datos";
-    }
-  }
-  function displayWheaterData(hourlyData,element){
-    element.textContent = "";  
-
-    hourlyData.time.forEach((time, index) => {
-        let card = document.createElement("div");
-        card.classList.add("card", "mb-3", "p-3");  // Clases de Bootstrap para las tarjetas
-        let cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-
-        let titleIndex = document.createElement("h3");
-        
-        let temperatureMaxP = document.createElement("p");
-        let temperatureMinP = document.createElement("p");
-        let temperatureMidP =document.createElement("p");
-       //style
-        temperatureMidP.classList.add("card-text");
-        temperatureMinP.classList.add("card-text");
-        temperatureMaxP.classList.add("card-text");
-        titleIndex.classList.add("card-title");
-        //parse
-        titleIndex.textContent = `Day: ${time}`;
-        temperatureMaxP.textContent = `Temperatura Max: ${hourlyData.temperature_2m_max[index]} °C`;
-        temperatureMinP.textContent = `Temperatura Min: ${hourlyData.temperature_2m_min[index]} °C`;
-        temperatureMidP.textContent = `Temperatura Mid: ${hourlyData.temperature_2m_mean[index]} °C`;
-        titleIndex.setAttribute("id","title-data")
-
-        cardBody.appendChild(titleIndex);
-        cardBody.appendChild(temperatureMaxP);
-        cardBody.appendChild(temperatureMinP);
-        cardBody.appendChild(temperatureMidP);
-        card.appendChild(cardBody);
-        element.appendChild(card)
-    });
-}
-
-const allTimes= document.querySelectorAll("#json-api-time");
-allTimes.forEach((element)=>{
-    console.log(element);
-    getTime(element.className,8,element);
 })
